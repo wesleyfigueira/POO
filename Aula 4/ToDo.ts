2f5cc3c9c47dd41e247dc57636de7e5a5f0d4c7c
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-03-06 21:51:25 -0300

    to

---
+++ Aula 4/ToDo.ts
@@ -0,0 +1,24 @@
+
+class tarefa{
+    tarefa:string;
+    id:number;
+    listaDetarefas?: string[]=[]
+
+    constructor( tarefa:string,id:number,){
+        tarefa =this.tarefa
+        id = this.id
+    }
+
+
+
+}
+
+class CriarTarefa{
+
+    constructor(id:number, tarefa:string){
+        id= this.id.length +1
+        let novatarefa= this.tarefa
+        this.listaDeTarefa.push(novatarefa);
+    }
+
+    }
