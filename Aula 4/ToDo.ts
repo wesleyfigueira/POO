ed46a0b5b181920ac9606ef46af070ecedcd82f4
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-03-08 21:08:07 -0300

    tentativa todo list

--- Aula 4/ToDo.ts
+++ Aula 4/ToDo.ts
@@ -2,25 +2,3 @@
 //concluir tarefa
 // Remover uma tarefa
 //imprimir lista de tarefas
-
-
-class ListaDeTarefas {
-
-    tarefa: string;
-    id:number;
-    status:boolean;
-
-    constructor(tarefa:string, id:number,status:boolean) {
-        this.tarefa =tarefa;
-        this.id =id;
-        this.status=status
-    }
-
-    task(){
-        console.log(`${this.modelo}`)
-    }
-}
-
-const meucarro = new Veiculo("polo",30,"volkswagen");
-
-meucarro.montar()
