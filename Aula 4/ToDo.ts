678f07fa8456e2fd0d4da9b0fcf642b18948076b
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-03-08 21:23:27 -0300

    Adicionar to do list

--- Aula 4/ToDo.ts
+++ Aula 4/ToDo.ts
@@ -1,4 +1,25 @@
 //Criar Nova Tarefa
+
+class ToDoList{
+
+    taskList:string [];
+
+    constructor(){
+        this.taskList =[]
+    }
+
+    addTask(tarefa:string){
+        this.taskList.push(tarefa)
+        console.log(`Tarefa "${tarefa}" criada.`);
+
+    }
+}
+
+const listaDeTarefas = new ToDoList();
+listaDeTarefas.addTask("comprar Pão");
+listaDeTarefas.addTask("comprar Ovo");
+listaDeTarefas.addTask("comprar Manteiga");
 //concluir tarefa
 // Remover uma tarefa
 //imprimir lista de tarefas
+
